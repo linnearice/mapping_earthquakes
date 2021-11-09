62ab5486e4892abdd683f568dbcba61c292185f1
Linnea Rice <35401581+linnearice@users.noreply.github.com>
2021-11-09 16:47:28 -0600

    update files

--- Earthquake_Challenge/static/js/logicStep3.js
+++ Earthquake_Challenge/static/js/logicStep3.js
@@ -21,15 +21,6 @@ let baseMaps = {
   "Satellite Streets": satelliteStreets
 };
 
-// Create the earthquake layer for our map.
-let earthquakes = new L.layerGroup();
-
-// We define an object that contains the overlays.
-// This overlay will be visible all the time.
-let overlays = {
-  Earthquakes: earthquakes
-};
-
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
   center: [39.5, -98.5],
@@ -38,8 +29,7 @@ let map = L.map('mapid', {
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
-// We add a control to the map that will allow the user to change which layers are visible.
-L.control.layers(baseMaps, overlays).addTo(map);
+L.control.layers(baseMaps).addTo(map);
 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
@@ -101,36 +91,7 @@ style: styleInfo,
   onEachFeature: function(feature, layer) {
   layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
 }
-}).addTo(earthquakes);
-
-// Create a legend control object.
-let legend = L.control({
-  position: "bottomright"
-});
-
-// Then add all the details and colors for the legend.
-legend.onAdd = function(){
-  let div = L.DomUtil.create("div", "info legend");
-    const magnitudes = [0, 1, 2, 3, 4, 5];
-    const colors = [
-    "#98ee00",
-    "#d4ee00",
-    "#eecc00",
-    "#ee9c00",
-    "#ea822c",
-    "#ea2c2c"
-  ];
-  // Looping through our intervals to generate a label with a colored square for each interval.
-  for (var i = 0; i < magnitudes.length; i++) {
-    console.log(colors[i]);
-    div.innerHTML +=
-      "<i style='background: " + colors[i] + "'></i> " +
-      magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
- }
-  return div;
-};
-
-legend.addTo(map);
+}).addTo(map);
 
 });
 
